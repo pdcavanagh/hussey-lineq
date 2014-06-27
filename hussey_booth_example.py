50681fc5d377727489cfc1fb3d058e302b31916b
Patrick Cavanagh <pdcavanagh@yahoo.com>
2014-06-27 16:42:56 -0400

    Updated Hussey example to iterative approach to reduce residual and arrive at R^2 <0.001 for all three samples.

--- hussey_booth_example.py
+++ hussey_booth_example.py
@@ -14,7 +14,8 @@
 #******************************************************************************
 import numpy as np
 
-debug=0
+debug=1
+results=1
 
 a = np.matrix([[14,5,5],[10,100,160],[10,100,10]])
 b = np.matrix([[11.3,6.8,6.8],
@@ -36,37 +37,50 @@ if debug:
     print a.getI()*b
     print np.round(a.getI()*b_mod,2)
 
-r_sqr=0 # Global residual value
-r = a*e - b[:,0]
+
+thresh=0.001
+r = a*e - b_mod
 if debug:
     print r
 
 a_prime = a.getT()
 
 # Calculate the residual over all of the phases property measurements
-for i in r:
-    if debug:
-        print r_sqr 
-    r_sqr = i*i + r_sqr
+for n,i in enumerate(r.getT()):
+    r_sqr=1 # Global residual value
+    if results:
+        print ('\n***************Phase %s***************\n' % n)
+        print i
+    i=i.getT()
+    count=0 
+    while r_sqr>thresh:
+        count=count+1
+        correct_2 = a.getT()*i
+        if debug: print ('correct_2:\n%s' % correct_2)
+        correct_3 = a*correct_2
+        if debug: print 'correct_3:'
+        if debug: print correct_3
+        correct_4 = i.getT()*correct_3
+        if debug: print 'correct_4:'
+        if debug: print correct_4
 
-if debug: print 'Global residual value (R^2): {0:5.2f}'.format(float(r_sqr))
+        correct_denom = sum(np.square(correct_3))
+        if debug: print ('correct_denom: %s' % correct_denom)
+        correct_5 = correct_4/correct_denom  
+        if debug: print 'correct_4/correct_denom: {}'.format(correct_5)
 
-correct_2 = a.getT()*r
-if debug: print ('correct_2:\n%s' % correct_2)
-correct_3 = a*correct_2
-if debug: print 'correct_3:'
-if debug: print correct_3
-correct_4 = r.getT()*correct_3
-if debug: print r
-if debug: print 'correct_4:'
-if debug: print correct_4
+        correct_term = np.multiply(correct_5,correct_2)
+        if debug: print correct_term
+        e=e-correct_term #update the approximation vector 
+        i = a*e - b_mod[:,n]
 
-correct_denom = sum(np.square(correct_3))
-if debug: print ('correct_denom: %s' % correct_denom)
-correct_5 = correct_4/correct_denom  
-if debug: print 'correct_4/correct_denom: {}'.format(correct_5)
-
-correct_term = np.multiply(correct_5,correct_2)
-if debug: print correct_term
-print e-correct_term
-
+        r_sqr=0 #reset the r_sqr value
+        for resid in i:
+            if debug:
+                print resid 
+                print r_sqr
+            r_sqr = resid*resid + r_sqr
+    if results: 
+        print ('Global residual value (R^2): {0:5.2f}'.format(float(r_sqr)))
+        print ('The number of iterations to achieve <0.01 R^2: %s' % count)
+        print ('The estimated phase abundaces are:\n%s' % np.round(e,3))
